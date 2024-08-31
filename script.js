082488a1029e6f1dbd662d97eae529face5a5c88
sanjay8699 <sanjay8699newbe@gmail.com>
2024-08-31 16:30:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* nav script */
+
+var sidenav = document.querySelector(".side-navbar")
+
+function shownavbar() {
+    sidenav.style.left = "0%"
+}
+
+function closenavbar() {
+    sidenav.style.left = "-60%"
+}
